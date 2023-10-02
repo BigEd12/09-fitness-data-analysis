9285e5e67f7f3a52550045a596a2c3496af0dbc2
Eduardo Olmos <olmos.edward@gmail.com>
2023-10-03 00:45:43 +0200

    add average speed and high speed lines

--- utils/data_visualisation.py
+++ utils/data_visualisation.py
@@ -9,7 +9,7 @@ def create_time_distance_graph(df):
 
     fig.add_trace(
         go.Scatter(x=df['Total Time (M)'], y=df['Total Distance (M)'],
-                   line=dict(width=3, color='red'),
+                   line=dict(width=1, color='red'),
                    hovertext=['Time={} min, Distance={} Km'.format(round(time, 1), round(distance / 1000, 2))
                               for i, (time, distance) in enumerate(zip(df['Total Time (M)'], df['Total Distance (M)']))],
                    mode='lines+markers',
@@ -29,14 +29,14 @@ def create_time_distance_graph(df):
                 sizex=x_lims,
                 sizey=y_lims,
                 sizing="stretch",
-                opacity=0.8,
+                opacity=0.7,
                 layer="below")
     )
 
     fig.update_layout(xaxis_showgrid=False, yaxis_showgrid=False)
 
-    fig.update_xaxes(title_text='Time (minutes)')
-    fig.update_yaxes(title_text='Distance (Km)')
+    fig.update_xaxes(title_text='Time (minutes)', range=[0, x_lims])
+    fig.update_yaxes(title_text='Distance (Km)', range=[0, y_lims])
 
     fig.update_layout(title_text='Distance Over Time')
 
@@ -50,7 +50,7 @@ def create_altitude_distance_graph(df):
 
     fig.add_trace(
         go.Scatter(x=df['Total Distance (M)'], y=df['Altitude (M)'],
-                   line=dict(width=3, color='red'),
+                   line=dict(width=1, color='red'),
                    hovertext=['Distance={} Km, Altitude={} M'.format(round(distance / 1000, 2, ), round(altitude, 2))
                               for i, (distance, altitude) in enumerate(zip(df['Total Distance (M)'], df['Altitude (M)']))],
                    mode='lines+markers',
@@ -70,14 +70,14 @@ def create_altitude_distance_graph(df):
                 sizex=x_lims,
                 sizey=y_lims,
                 sizing="stretch",
-                opacity=0.8,
+                opacity=0.7,
                 layer="below")
     )
 
     fig.update_layout(xaxis_showgrid=False, yaxis_showgrid=False)
 
-    fig.update_xaxes(title_text='Distance (Km)')
-    fig.update_yaxes(title_text='Altitude (M)')
+    fig.update_xaxes(title_text='Distance (Km)', range=[0, x_lims])
+    fig.update_yaxes(title_text='Altitude (M)', range=[0, y_lims])
 
     fig.update_layout(title_text='Altitude over Distance')
 
