4ff25ff86db872bc1f08afc7470435af89fed0e7
Eduardo Olmos <olmos.edward@gmail.com>
2023-10-02 20:44:41 +0200

    add altitude over distance graph function

--- utils/data_visualisation.py
+++ utils/data_visualisation.py
@@ -9,23 +9,25 @@ def create_time_distance_graph(df):
 
     fig.add_trace(
         go.Scatter(x=df['Total Time (M)'], y=df['Total Distance (M)'],
-                   line=dict(width=5, color='red'),
+                   line=dict(width=3, color='red'),
                    hovertext=['Time={} min, Distance={} Km'.format(round(time, 1), round(distance / 1000, 2))
                               for i, (time, distance) in enumerate(zip(df['Total Time (M)'], df['Total Distance (M)']))],
                    mode='lines+markers',
                    )
     )
-
-    # Add images
+    
+    x_lims = df.iloc[-1]['Total Time (M)'] * 1.1
+    y_lims = df.iloc[-1]['Total Distance (M)'] * 1.1
+    
     fig.add_layout_image(
             dict(
                 source=img,
                 xref="x",
                 yref="y",
                 x=0,
-                y=90000,
-                sizex=250,
-                sizey=90000,
+                y=y_lims,
+                sizex=x_lims,
+                sizey=y_lims,
                 sizing="stretch",
                 opacity=0.8,
                 layer="below")
@@ -37,5 +39,46 @@ def create_time_distance_graph(df):
     fig.update_yaxes(title_text='Distance (Km)')
 
     fig.update_layout(title_text='Distance Over Time')
+
+    return fig.show()
+
+def create_altitude_distance_graph(df):
+    image_path = "images/bg.jpg"
+    img = Image.open(image_path)
+    
+    fig = go.Figure()
+
+    fig.add_trace(
+        go.Scatter(x=df['Total Distance (M)'], y=df['Altitude (M)'],
+                   line=dict(width=3, color='red'),
+                   hovertext=['Distance={} Km, Altitude={} M'.format(round(distance / 1000, 2, ), round(altitude, 2))
+                              for i, (distance, altitude) in enumerate(zip(df['Total Distance (M)'], df['Altitude (M)']))],
+                   mode='lines+markers',
+                   )
+    )
+    
+    x_lims = df.iloc[-1]['Total Distance (M)'] * 1.1
+    y_lims = df['Altitude (M)'].max() * 1.1
+    
+    fig.add_layout_image(
+            dict(
+                source=img,
+                xref="x",
+                yref="y",
+                x=0,
+                y=y_lims,
+                sizex=x_lims,
+                sizey=y_lims,
+                sizing="stretch",
+                opacity=0.8,
+                layer="below")
+    )
+
+    fig.update_layout(xaxis_showgrid=False, yaxis_showgrid=False)
+
+    fig.update_xaxes(title_text='Distance (Km)')
+    fig.update_yaxes(title_text='Altitude (M)')
+
+    fig.update_layout(title_text='Altitude over Distance')
 
     return fig.show()
