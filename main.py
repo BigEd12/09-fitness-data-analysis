fa717f98e159c6eb24b920e9f72f644a7da79c8f
Eduardo Olmos <olmos.edward@gmail.com>
2023-10-10 09:10:09 +0200

    create flask and bootstrap files and folders

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 from utils import data_preparation, data_visualisation
 
+#---------------------- FLASK ----------------------#
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
+Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
